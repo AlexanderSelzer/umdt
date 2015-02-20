862f8878e8fa6ebc93a5b75badc1378748d4a489
Alexander Selzer <aselzer3@gmail.com>
2015-02-20 18:33:52 +0100

    made list look nicer

--- static/js/main.js
+++ static/js/main.js
@@ -4,8 +4,43 @@ L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
   attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-$.get("api/data", function(data) {
-  var devices = {}
+var dataView = $(".data")
+
+
+/* 
+ * Loading everything at once is not very scalable,
+ * and adding it all at once to the map makes the browser get
+ * stuck pretty badly.
+ */
+
+function addDataToMap(data) {
+  data.forEach(function(d) {
+    var coords = [d.lat, d.lon]
+    L.marker(coords).addTo(map)
+    .bindPopup(d.tracking_data.shost)
+  })
+}
+
+$.get("api/data/wifi/unique", function(uniqueDevices) {
+  uniqueDevices.sort(function(a, b) {
+    return b.count - a.count
+  }).forEach(function(device) {
+    var listItem = $("<div>").addClass("list-item")
+    listItem.append($("<div>").addClass("field").text(device.count))
+    listItem.append($("<div>").addClass("field").text(device.shost))
+    dataView.append(listItem)
+
+    listItem.on("click", function() {
+      $.get("/api/data/wifi/shost/" + device.shost, function(res) {
+        addDataToMap(res)
+      })
+    })
+  })
+})
+
+/*
+$.get("api/data", function(response) {
+  data = response
 
   data.forEach(function(d) {
     var mac = d.tracking_data.shost
@@ -25,4 +60,5 @@ $.get("api/data", function(data) {
         .bindPopup(mac)
     })
   })
-})  
+})
+*/
